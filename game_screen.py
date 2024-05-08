883b5b0f34ada5bffbd1de070e784aadb3b84e0e
Fernando Nakatsubo <fernandocn@al.insper.edu.br>
2024-05-08 08:48:00 -0300

    config

--- game_screen.py
+++ game_screen.py
@@ -1,1 +1,35 @@
-#funcao com o loop principal do jogo
+# ===== Inicialização =====
+# ----- Importa e inicia pacotes
+import pygame
+
+pygame.init()
+
+# ----- Gera tela principal
+window = pygame.display.set_mode((900, 600))
+pygame.display.set_caption('Hello World!')
+
+# ----- Inicia estruturas de dados
+game = True
+
+# ----- Inicia assets
+font = pygame.font.SysFont(None, 48)
+text = font.render('Flappy Tiago', True, (0, 0, 255))
+
+# ===== Loop principal =====
+while game:
+    # ----- Trata eventos
+    for event in pygame.event.get():
+        # ----- Verifica consequências
+        if event.type == pygame.QUIT:
+            game = False
+
+    # ----- Gera saídas
+    window.fill((255, 255, 255))  # Preenche com a cor branca
+    window.blit(text, (100, 100))
+
+    # ----- Atualiza estado do jogo
+    pygame.display.update()  # Mostra o novo frame para o jogador
+
+# ===== Finalização =====
+pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
+
---
+++ init_screen.py
@@ -0,0 +1,7 @@
+import pygame
+import random
+from os import path
+
+from config import IMG_DIR, BLACK, FPS, GAME, QUIT
+
+tela_inicio.JPEG
