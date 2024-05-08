2d963220b6480037637e6cb1f377e8d3b0f9bcdd
Fernando Nakatsubo <fernandocn@al.insper.edu.br>
2024-05-08 08:49:12 -0300

    config2

--- config.py
+++ config.py
@@ -1,3 +1,18 @@
 #coloquei medidas aleatorias
-WIDTH = 600 
-HEIGHT = 300
+from os import path
+
+# Estabelece a pasta que contem as figuras e sons.
+IMG_DIR = path.join(path.dirname(__file__), 'imagens')
+SND_DIR = path.join(path.dirname(__file__), 'imagens')
+FNT_DIR = path.join(path.dirname(__file__), 'imagens')
+
+
+WIDTH = 900 # Largura da tela 
+HEIGHT = 700 # Altura da tela
+FPS = 60 # Frames por segundo
+
+
+# Estados para controle do fluxo da aplicação
+INIT = 0
+GAME = 1
+QUIT = 2
