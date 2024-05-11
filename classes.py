0abaa4a38d4bee85612e2c27c761434a94f3f463
Helena <helenaac@al.insper.edu.br>
2024-05-11 11:34:16 -0300

    pulinhos tiago

--- classes.py
+++ classes.py
@@ -8,32 +8,32 @@ from config import *
 scroll_speed = 1
 
 class Tiago (pygame.sprite.Sprite): 
-    def __init__(self): 
+    def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = personagem 
         self.image = pygame.transform.scale(personagem, (65, 65))
-        self.rect = self.image.get_rect() 
-        self.rect.center = pos_inic_tiago 
-        self.vel = 0 
-        self.pulo = False 
+        self.rect = self.image.get_rect()
+        self.rect.center = pos_inic_tiago
+        self.vel = 0
+        self.flap = False
 
-    def update (self, usuario): 
-        # Pulinho 
-        self.vel += 0.5 
-        if self.vel > 7: 
-            self.vel = 7 
-        if self.rect.y < 500: 
+
+    def update(self, user_input):
+        # Gravity and Flap
+        self.vel += 0.5
+        if self.vel > 7:
+            self.vel = 7
+        if self.rect.y < 500:
             self.rect.y += int(self.vel)
-        if self.vel == 0: # Velocidade 0 = parte mais alta do pulo (usuário não consegue pular mais)
+        if self.vel == 0:
             self.pulo = False
-            
-        # Usuario 
-        if usuario[pygame.K_SPACE] and not self.pulo and self.rect.y > 0:
-            self.pulo = True 
-            self.vel = -7 
 
-        # Rotação Tiago 
+        # Rotate Bird
         self.image = pygame.transform.rotate(self.image, self.vel * -7)
+
+        # User Input
+        if user_input[pygame.K_SPACE] and not self.pulo and self.rect.y > 0:
+            self.pulo = True
+            self.vel = -7
 
 
 
