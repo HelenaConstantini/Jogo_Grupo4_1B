fed9774f42ead0f819a59396236ce04adfc9fd6a
Fernando Nakatsubo <fernandocn@al.insper.edu.br>
2024-05-13 11:25:24 -0300

    att coqueiros

--- logica_geral.py
+++ logica_geral.py
@@ -68,21 +68,23 @@ def main():
             x_top, x_bottom = 550, 550
             y_top = random.randint(-600,-480)
             y_bottom = y_top + random.randint(90,130) + coqueiro_img.get_height()
-            coqueiros.add(coqueiro(x_top, y_top, coqueiro_img))
+            coqueiros.add(coqueiro(x_top, y_top, nuvem_img))
             coqueiros.add(coqueiro(x_bottom,y_bottom,coqueiro_img))
             coqueiro_timer = random.randint(180,250)
         coqueiro_timer -= 1
-        all_sprites.add(coqueiros)
+        
 
 
         #atualiza chao, tiago e coqueiros
         all_sprites.update()
+        coqueiros.update()
 
         #fundo de tela
         screen.blit(fundo_blur_img, (0, 0)) #recebe dois argumentos: uma imagem e as coordenadas
 
         #desenha chao,tiago e coqueiros
         all_sprites.draw(screen)
+        coqueiros.draw(screen)
 
 
         clock.tick(FPS)
